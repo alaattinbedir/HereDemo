571ce754f159e4e871bd9210b6d3a5aadd386cf4
Alaattin Bedir <alaattinbedir@gmail.com>
2019-03-15 09:48:32 +0300

    Initial Commit

---
+++ HereDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HereDemo
+//
+//  Created by Melike Sardogan on 15.03.2019.
+//  Copyright © 2019 Alaattin Bedir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HereDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HereDemo
+//
+//  Created by Melike Sardogan on 15.03.2019.
+//  Copyright © 2019 Alaattin Bedir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
