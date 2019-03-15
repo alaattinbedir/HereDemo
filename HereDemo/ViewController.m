b50e4e30ddd9a695e68c453c678fd5a88ee59e13
Alaattin Bedir <alaattinbedir@gmail.com>
2019-03-15 12:12:56 +0300

    amend

--- HereDemo/ViewController.m
+++ HereDemo/ViewController.m
@@ -13,12 +13,17 @@
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet NMAMapView *mapView;
 
+
 @end
 
-@implementation ViewController
+@implementation ViewController {
+    NSMutableArray<NMAMapMarker*>* _mapMarkers;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    _mapMarkers = [NSMutableArray array];
     
     //set geo center
     NMAGeoCoordinates *geoCoordCenter =
@@ -32,16 +37,16 @@
     
     
     // register for position changes
-    if ([[NMAPositioningManager sharedPositioningManager] startPositioning]) {
-        // Register to positioning manager notifications
-        [[NSNotificationCenter defaultCenter] addObserver:self
-                                                 selector:@selector(positionDidUpdate) name:NMAPositioningManagerDidUpdatePositionNotification
-                                                   object:[NMAPositioningManager sharedPositioningManager]];
-        
-        [[NSNotificationCenter defaultCenter] addObserver:self
-                                                 selector:@selector(didLosePosition) name: NMAPositioningManagerDidLosePositionNotification
-                                                   object:[NMAPositioningManager sharedPositioningManager]];
-    }
+//    if ([[NMAPositioningManager sharedPositioningManager] startPositioning]) {
+//        // Register to positioning manager notifications
+//        [[NSNotificationCenter defaultCenter] addObserver:self
+//                                                 selector:@selector(positionDidUpdate) name:NMAPositioningManagerDidUpdatePositionNotification
+//                                                   object:[NMAPositioningManager sharedPositioningManager]];
+//
+//        [[NSNotificationCenter defaultCenter] addObserver:self
+//                                                 selector:@selector(didLosePosition) name: NMAPositioningManagerDidLosePositionNotification
+//                                                   object:[NMAPositioningManager sharedPositioningManager]];
+//    }
     // Set position indicator visible. Also starts position updates.
     self.mapView.positionIndicator.visible = YES;
     
@@ -91,5 +96,48 @@
     [self positionDidUpdate];
 }
 
+- (IBAction)createRoute:(id)sender {
+    
+    NMAGeoCoordinates *geoCoordCenter = [[NMAGeoCoordinates alloc] initWithLatitude:41.000144 longitude:29.099402];
+
+    NMAMapMarker *altemiraMark = [NMAMapMarker mapMarkerWithGeoCoordinates:geoCoordCenter image:[UIImage imageNamed:@"logo.png"]];
+    
+    [self.mapView addMapObject:altemiraMark];
+    
+    [_mapMarkers addObject:altemiraMark];
+    
+    
+    [self navigateToMarkers];
+}
+
+-(void)navigateToMarkers {
+    // find max and min latitudes and longitudes in order to calculate
+    // geo bounding box so then we can mapView->setBoundingBox(geoBox, ...) to it.
+    double minLat = 90.0;
+    double minLon = 180.0;
+    double maxLat = -90.0;
+    double maxLon = -180.0;
+    
+    for (NMAMapMarker* marker in _mapMarkers) {
+        NMAGeoCoordinates* coordinate = marker.coordinates;
+        
+        double latitude = coordinate.latitude;
+        double longitude = coordinate.longitude;
+        
+        minLat = fmin(minLat, latitude);
+        minLon = fmin(minLon, longitude);
+        maxLat = fmax(maxLat, latitude);
+        maxLon = fmax(maxLon, longitude);
+    }
+    
+    NMAGeoBoundingBox* box = [NMAGeoBoundingBox geoBoundingBoxWithTopLeft:[NMAGeoCoordinates geoCoordinatesWithLatitude:maxLat longitude:minLon]
+                                                              bottomRight:[NMAGeoCoordinates geoCoordinatesWithLatitude:minLat longitude:maxLon]];
+    CGFloat padding = 50;
+    [[self mapView] setBoundingBox:box
+                        insideRect:CGRectMake(padding, padding, self.mapView.bounds.size.width - padding * 2, self.mapView.bounds.size.height - padding * 2)
+                     withAnimation:NMAMapAnimationLinear];
+    
+    self.mapView.zoomLevel = 13.2;
+}
 
 @end
